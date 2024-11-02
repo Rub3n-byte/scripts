0c0bd2f251e297b832b4fe7e7ec100d74885e810
Rubén Buenaventura Rigol Peña <cuentadevestudiorub3n@gmail.com>
2024-11-02 19:14:52 +0100

    IP

--- bash/static_ip.sh
+++ bash/static_ip.sh
@@ -7,12 +7,14 @@
 
 #Pedir la IP y establecerla
 echo "Introduce la IP de la máquina"
-read IP0
+read IP0base
+IP0="\taddress $IP0base"
 echo "Introduce la máscara de red"
-read MASK0
+read MASK0base
 echo "Introduce la puerta de enlace"
-read GATEWAY0
-echo "Introduce el DNS primario, si no tienes, introduce "_""
-read DNS01
-echo "Introduce el DNS secundario, si no tienes, introduce "_""
-read DNS02
+read GATEWAY0base
+echo "Introduce el DNS primario, si no tienes, introduce "_", o la puerta de enlace para uno dinamico"
+read DNS01base
+echo "Introduce el DNS secundario, si no tienes, introduce "_", o la puerta de enlace para uno dinamico"
+read DNS02base
+IPstatic=IP0+MASK0+GATEWAY0
